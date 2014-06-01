85b87f78dfe47d1b18084c3284b86ddff3fed3e9
KIM Taegyoon <steloflute@gmail.com>
2014-06-02 00:39:57 +0900

    efficient

--- SweepSecond.rkt
+++ SweepSecond.rkt
@@ -40,7 +40,7 @@
   (: dc set-brush brush)
   (: dc draw-polygon (list pFrom pSide1 pTo pSide2)))
 
-(define (drawTick dc center radius length radians pen)
+(define (drawTick dc center radius length radians)
   (define cx (: center get-x))
   (define cy (: center get-y))
   (define pTo (make-object point%
@@ -49,57 +49,56 @@
   (define pFrom (make-object point%
                   (+ cx (* radius (- 1 length) (sin radians)))
                   (+ cy (* radius (- 1 length) (cos radians)))))
-  (: dc set-pen pen)
   (: dc draw-lines (list pTo pFrom)))
 
 (define (paint canvas dc)
   (: dc set-background "black")
   (: dc clear)
   (: dc set-smoothing 'smoothed)
-  
+
   (define now (current-date))
   (define hour (date-hour now))
   (define minute (date-minute now))
   (define second (+ (date-second now) (- (/ (current-inexact-milliseconds) 1000) (current-seconds))))
-  
+
   (: dc set-text-foreground "white")
   ;  (: dc draw-text (date->string now #t) 0 0)
   (: frame set-status-text (date->string now #t))
-  
+
   (define-values (w h) (: dc get-size))
   (define r (/ (min w h) 2))
   (define center (make-object point% (/ w 2) (/ h 2)))
-  
+
   (define radSec (toRadians (/ second 60)))
   (define radMin (toRadians (/ (+ minute (/ second 60)) 60)))
   (define radHour (toRadians (/ (+ (remainder hour 12) (/ minute 60)) 12)))
-  
+
   ; minute ticks
-  (define pen (make-object pen% "white" (/ r 120)))
-  (for ([rad (range 0 (* pi 2) (/ (* pi 2) 60))])
-    (drawTick dc center (* r 0.95) 0.06 rad pen))
-  
+  (: dc set-pen (make-object pen% "white" (/ r 120)))
+  (for ([rad (in-range 0 (* pi 2) (/ (* pi 2) 60))])
+    (drawTick dc center (* r 0.95) 0.06 rad))
+
   ; hour ticks
-  (set! pen (make-object pen% "white" (/ r 60)))
-  (for ([rad (range 0 (* pi 2) (/ (* pi 2) 12))])
-    (drawTick dc center (* r 0.95) 0.2 rad pen))
-  
+  (: dc set-pen (make-object pen% "white" (/ r 60)))
+  (for ([rad (in-range 0 (* pi 2) (/ (* pi 2) 12))])
+    (drawTick dc center (* r 0.95) 0.2 rad))
+
   ; 0 3 6 9 ticks
-  (set! pen (make-object pen% "white" (/ r 30)))
-  (for ([rad (range 0 (* pi 2) (/ (* pi 2) 4))])
-    (drawTick dc center (* r 0.95) 0.2 rad pen))
-  
+  (: dc set-pen (make-object pen% "white" (/ r 30)))
+  (for ([rad (in-range 0 (* pi 2) (/ (* pi 2) 4))])
+    (drawTick dc center (* r 0.95) 0.2 rad))
+
   ; rim
   (: dc set-pen "white" (/ r 60) 'solid)
   (: dc set-brush "" 'transparent)
   (: dc draw-ellipse (- (: center get-x) r) (- (: center get-y) r) (* r 2) (* r 2))
-  
+
   ; hands
   (: dc set-pen "" 0 'transparent)
   (drawHand dc center (* r 0.5) radHour brushHour (/ 1.1 0.5))
   (drawHand dc center (* r 0.8) radMin brushMin (/ 1.0 0.8))
   (drawHand dc center (* r 0.9) radSec brushSec (/ 0.5 0.9))
-  
+
   ; center
   (define r2 (/ r 20))
   (: dc set-brush "white" 'solid)
